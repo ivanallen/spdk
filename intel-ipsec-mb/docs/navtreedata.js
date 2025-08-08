3a6e3b71fab481ac8a0999b0b1d35aa6b6150c24
allen <1007729991@qq.com>
2025-08-08 13:26:39 +0800

    add intel-ipsec-mb
    Signed-off-by: allen <1007729991@qq.com>

---
+++ intel-ipsec-mb/docs/navtreedata.js
@@ -0,0 +1,111 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "intel-ipsec-mb", "index.html", [
+    [ "Intel(R) Multi-Buffer Crypto for IPsec Library", "index.html#autotoc_md0", [
+      [ "Contents", "index.html#autotoc_md1", null ],
+      [ "1. Overview", "index.html#autotoc_md2", null ],
+      [ "2. Processor Extensions", "index.html#autotoc_md3", null ],
+      [ "3. Recommendations", "index.html#autotoc_md4", null ],
+      [ "4. Package Content", "index.html#autotoc_md5", null ],
+      [ "5. Documentation", "index.html#autotoc_md6", null ],
+      [ "6. Compilation", "index.html#autotoc_md7", [
+        [ "Linux (64-bit only)", "index.html#autotoc_md8", null ],
+        [ "Windows MSVS (x64 only)", "index.html#autotoc_md9", null ],
+        [ "Windows Mingw-w64 (64-bit only)", "index.html#autotoc_md10", null ],
+        [ "FreeBSD (64-bit only)", "index.html#autotoc_md11", null ],
+        [ "Building with CMake (experimental)", "index.html#autotoc_md12", [
+          [ "Unix Makefiles (Linux and FreeBSD)", "index.html#autotoc_md13", null ],
+          [ "Windows MSVS (x64 only)", "index.html#autotoc_md14", null ],
+          [ "Ninja (Linux, FreeBSD and Windows):", "index.html#autotoc_md15", null ]
+        ] ]
+      ] ],
+      [ "7. Security Considerations & Options for Increased Security", "index.html#autotoc_md16", [
+        [ "Security Considerations", "index.html#autotoc_md17", null ],
+        [ "Options for Increased Security", "index.html#autotoc_md18", null ],
+        [ "SAFE_DATA", "index.html#autotoc_md19", null ],
+        [ "SAFE_PARAM", "index.html#autotoc_md20", null ],
+        [ "SAFE_LOOKUP", "index.html#autotoc_md21", null ],
+        [ "SAFE_OPTIONS", "index.html#autotoc_md22", null ],
+        [ "Security API", "index.html#autotoc_md23", null ],
+        [ "Galois Counter Mode (GCM) TAG Size", "index.html#autotoc_md24", null ]
+      ] ],
+      [ "8. Installation", "index.html#autotoc_md25", [
+        [ "Linux (64-bit only)", "index.html#autotoc_md26", null ],
+        [ "Windows (x64 only)", "index.html#autotoc_md27", null ],
+        [ "FreeBSD (64-bit only)", "index.html#autotoc_md28", null ]
+      ] ],
+      [ "Installing with CMake (experimental)", "index.html#autotoc_md29", [
+        [ "Unix (Linux and FreeBSD)", "index.html#autotoc_md30", null ],
+        [ "Windows (x64 only)", "index.html#autotoc_md31", null ]
+      ] ],
+      [ "9. Backwards compatibility", "index.html#autotoc_md32", null ],
+      [ "10. Disclaimer (ZUC, KASUMI, SNOW3G)", "index.html#autotoc_md33", null ],
+      [ "11. Legal Disclaimer", "index.html#autotoc_md34", null ],
+      [ "12. FIPS Compliance", "index.html#autotoc_md35", [
+        [ "CAVP", "index.html#autotoc_md36", null ],
+        [ "CAVP Algorithm Parameters", "index.html#autotoc_md37", null ],
+        [ "Self-Test", "index.html#autotoc_md38", null ]
+      ] ],
+      [ "13.DLL Injection Attack", "index.html#autotoc_md39", [
+        [ "Problem", "index.html#autotoc_md40", null ],
+        [ "Solutions", "index.html#autotoc_md41", null ],
+        [ "Resources and Solution Details", "index.html#autotoc_md42", null ]
+      ] ]
+    ] ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"intel-ipsec-mb_8h.html#a437de868b8dba4f9b878d99a82f80548",
+"intel-ipsec-mb_8h.html#aa12c6dbc4b212887ef528c215f513e0ba0d35070d6476cde0202deb5531ae09c2",
+"structIMB__JOB.html#a4c943fcac7f5c4f5d6d030547840071e",
+"structkasumi__key__sched__s.html#a7e69de411b81753c10776442bbe03100"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
